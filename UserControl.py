b19c345919424e312232e8c4ad2a22be2163950d
Moonen <903284186@qq.com>
2019-05-09 21:18:16 +0800

    finished

--- UserControl.py
+++ UserControl.py
@@ -20,11 +20,59 @@ def login(username,password):
 
 def register(username,password,email):
 	try:
-		nowid = len(user.find())+1;
-		nowUser = {"_id": nowid, "username": username,"password": password};
+		chkUser = user.find_one({"username":username});
+		if (chkUser != None):
+			return 0;
+
+		nowid = user.find().count()+1;
+		nowUser = {
+			"_id": nowid, 
+			"username": username,
+			"password": password, 
+			"email": email,
+			"name" : "",
+			"organization" : "",
+			"introduction" : "",
+			"scholarID" : 0,
+			"subscribe" : [],
+			"collection" : [],
+			"purchased" : []
+		};
 		result = user.insert_one(nowUser);
 
 		print(result);
 		return nowid;
 	except:
 		return 0;
+
+def find(userID):
+	try:
+		nowUser = user.find_one({"_id":userID});
+
+		if not nowUser:
+			return None;
+
+		return nowUser;
+	except:
+		return None;
+
+def editInfo(userID,introduction,organization):
+	try:
+		user.update_one({"_id":userID}, { "$set": {"introduction": introduction, "organization": organization} });
+
+		return True;
+	except:
+		return False;
+
+def changePassword(userID,oldPassword,newPassword):
+	try:
+		if not user.find_one({"_id":userID,"password":oldPassword}):
+			return False;
+
+		user.update_one({"_id":userID,"password":oldPassword}, { "$set": {"password":newPassword} });
+
+		return True;
+	except:
+		return False;
+
+print(register("z242aw","1111","!24"))
