ad150e61c417980e164c7f252c706cf1b1d8e66d
Moonen <903284186@qq.com>
2019-05-19 17:27:57 +0800

    20190519-17:27

--- ResourceControl.py
+++ ResourceControl.py
@@ -6,18 +6,20 @@ mydb = myclient["test"]
 mycol = mydb["commentList"]
 
 def comment(userID, resourceID, content):
-    try:
-    	nowtime = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
-        mycol.insert_one({"userID":userID, "resourceID":resourceID,"content":content,"time":nowtime})
-    except pymongo.errors.DuplicateKeyError:
-        return False
-    return True
+	try:
+		nowtime = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
+		mycol.insert_one({"userID":userID, "resourceID":resourceID,"content":content,"time":nowtime})
+	except pymongo.errors.DuplicateKeyError:
+		return False
+	return True
 
 def findComment(resourceID):
 	try:
 		results = mycol.find({"resourceID" : resourceID})
 		if (results.count()):
 			return list(results)
-		return None
+		return []
 	except:
 		return None
+
+print(findComment(2))
