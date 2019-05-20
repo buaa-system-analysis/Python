e16d4236978e776532e89c63b0276d0d39bade05
Moonenzyw <903284186@qq.com>
2019-05-20 09:49:27 +0800

    Add files via upload

--- ScholarControl.py
+++ ScholarControl.py
@@ -5,6 +5,7 @@ import pymongo
 myclient = pymongo.MongoClient("mongodb://106.14.150.33:27017/")
 db = myclient['test']
 scholar = db['scholar']
+paper = db['Paper']
 user = db['user']
 
 
@@ -23,7 +24,68 @@ def authenticate(userID, email):
             return False
         if len(email)>7:
             if re.match("^.+\\@(\\[?)[a-zA-Z0-9\\-\\.]+\\.([a-zA-Z]{2,3}|[0-9]{1,3})(\\]?)$", email) != None:
+                scholar_id = scholar.find().count() + 1
+                new_scholar = {
+                    "_id": scholar_id, 
+                    "name": "",
+                    "email": email,
+                    "organization":"",
+                    "researchField":"",
+                    "researchTopic":"",
+                    "citation":"",
+                    "pubNumber":"",
+                    "h-Index":"",
+                    "coAuthor":"",
+                    "coOrg":""
+                };
+                scholar.insert_one(new_Scholar);
+                user.update_one({"_id":userID},{"$set":{"scholarID":scholar_id}})
                 return True
             return False
     except:
         return False
+
+def manageResource(resourceID,cmd,newPrice):
+    try:
+        if(cmd == 1):
+            paper.update_one({"_id":resourceID},{"$set":{"price":newPrice}})
+            return True
+        else:
+            return False
+    except:
+        return False
+
+def findScholar(scholarID):
+    try:
+        now_scholar = scholar.find_one({"_id":scholarID})
+        return now_scholar
+    except:
+        return None
+
+def findscholarByName(name):
+    try:
+        result = []
+        pattern = '.*'.join(name)
+        regex = re.compile(pattern)
+        collection = scholar.find()
+        for item in collection:
+            match = regex.search(item)
+            if match:
+                result.append(item)
+        return result
+    except:
+        return None
+
+def deleteScholar(scholarID):
+    try:
+        scholar.delete_one({"_id":scholarID})
+        user.update_one({"scholarID":scholarID},{"$set":{"scholarID":0}})
+        return True
+    except:
+        return False
+
+
+# print(findscholarByName("vic"))
+# print(findScholar(1))
+# print(deleteScholar(1))
+
