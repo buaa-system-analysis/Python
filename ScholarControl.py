bc74a9fab8284f88a34d9f9aff5f5d66d1e7aac4
Moonenzyw <903284186@qq.com>
2019-05-20 16:49:14 +0800

    Add files via upload

--- ScholarControl.py
+++ ScholarControl.py
@@ -7,42 +7,55 @@ db = myclient['test']
 scholar = db['scholar']
 paper = db['Paper']
 user = db['user']
+cklist = db['checkList']
 
 
-def editScholarInfo(scholarID, name, organization, resourceField):
+def editScholarInfo(scholarID, data):
     try:
-        scholar.update_one({"_id": scholarID}, {"$set": { "name": name, "organization": organization, "resourceField": resourceField}})
+        scholar.update_one({"_id": scholarID}, {"$set": data})
         return True
     except:
         return False
 
-def authenticate(userID, email):
+def authenticate(userID,scholarID, email):
     try:
         user_now = user.find_one({"_id":userID})
         if not user_now:
             return False
+        scholar_now = scholar.find_one({"_id":scholarID})
+        if not scholar_now:
+            return False
+        
         if len(email)>7:
             if re.match("^.+\\@(\\[?)[a-zA-Z0-9\\-\\.]+\\.([a-zA-Z]{2,3}|[0-9]{1,3})(\\]?)$", email) != None:
-                scholar_id = scholar.find().count() + 1
-                new_Scholar = {
-                    "_id": scholar_id, 
-                    "name": "",
-                    "email": email,
-                    "organization":"",
-                    "researchField":"",
-                    "researchTopic":"",
-                    "citation":"",
-                    "pubNumber":"",
-                    "h-Index":"",
-                    "coAuthor":"",
-                    "coOrg":""
-                };
-                scholar.insert_one(new_Scholar);
-                user.update_one({"_id":userID},{"$set":{"scholarID":scholar_id}})
+                cklist.insert_one({"userID":userID,"scholarID":scholarID,"email":email,"status":"unfinished"})
                 return True
             return False
     except:
         return False
+
+def addScholar(name):
+    try:
+        scholar_id = scholar.find().count() + 1
+        new_Scholar = {
+            "_id": scholar_id, 
+            "name": name,
+            "email": "",
+            "organization":"",
+            "fields":[],
+            "citation":0,
+            "h_index":0,
+            "g_index":0,
+            "papers":[],
+            "projects":[],
+            "patents":[],
+            "coAuthors":[],
+            "coOrgs":[]
+        };
+        scholar.insert_one(new_Scholar);
+        return scholar_id
+    except:
+        return 0
 
 def manageResource(resourceID,cmd,newPrice):
     try:
@@ -61,7 +74,7 @@ def findScholar(scholarID):
     except:
         return None
 
-def findscholarByName(name):
+def findScholarByName(name):
     try:
         results = scholar.find({"name" : { "$regex" : ".*" + name + ".*" } })
         if (results.count()):
@@ -78,6 +91,9 @@ def deleteScholar(scholarID):
     except:
         return False
 
+
+#print(findScholarByName(""))
+#print(authenticate(1,1,"a222@sdf.cc"))
 #print(authenticate(1,"c111@11.cc"))
 # print(findScholar(1))
 # print(deleteScholar(1))
