40b5d6ec61cf5f3355c465bc040ebb02fe184018
Don107213 <Hzdong0313@buaa.edu.cn>
2019-06-02 21:42:00 +0800

    initial Database

--- AddDatas.py
+++ AddDatas.py
@@ -27,53 +27,53 @@ for i,d in enumerate(data):
     print(addScholars(id, name, org, citation, h_index, papers, fields))
 
 
-succeed = 0
-ids = []
-data = open('../PaperData/aminer_papers_0.txt')
-for i,d in enumerate(data):
-    if i == 50000:
-        print(succeed)
-        print(ids)
-        break
-    elif i%10000 == 0:
-        print('i:',i,'succeed:',succeed)
-    try:
-        paper = json.loads(d)
-        id, title, price = paper['id'], paper['title'], i % 86
-        try:
-            citation = paper['n_citation']
-        except:
-            citation = 0
-        try:
-            fulltextURL = paper['pdf']
-        except:
-            fulltextURL = ''
-        field = []
-        try:
-            field = paper['keywords']
-        except:
-            pass
-        publishment = ''
-        try:
-            publishment = paper['doi']
-        except:
-            pass
-        abstract = ''
-        try:
-            abstract = paper['abstract']
-        except:
-            pass
-        authors = []
-        try:
-            for author in paper['authors']:
-                authors.append(author['name'])
-        except:
-            continue
-        if len(authors) == 0 or len(field) == 0 or abstract == '' or publishment == '':
-            continue
-        addPapers(id, title, authors, abstract, publishment, citation, field, price, fulltextURL)
-        ids.append(id)
-        succeed += 1
-    except:
-        print(i)
-        print(ids)
+# succeed = 0
+# ids = []
+# data = open('../PaperData/aminer_papers_0.txt')
+# for i,d in enumerate(data):
+#     if i == 50000:
+#         print(succeed)
+#         print(ids)
+#         break
+#     elif i%10000 == 0:
+#         print('i:',i,'succeed:',succeed)
+#     try:
+#         paper = json.loads(d)
+#         id, title, price = paper['id'], paper['title'], i % 86
+#         try:
+#             citation = paper['n_citation']
+#         except:
+#             citation = 0
+#         try:
+#             fulltextURL = paper['pdf']
+#         except:
+#             fulltextURL = ''
+#         field = []
+#         try:
+#             field = paper['keywords']
+#         except:
+#             pass
+#         publishment = ''
+#         try:
+#             publishment = paper['doi']
+#         except:
+#             pass
+#         abstract = ''
+#         try:
+#             abstract = paper['abstract']
+#         except:
+#             pass
+#         authors = []
+#         try:
+#             for author in paper['authors']:
+#                 authors.append(author['name'])
+#         except:
+#             continue
+#         if len(authors) == 0 or len(field) == 0 or abstract == '' or publishment == '':
+#             continue
+#         addPapers(id, title, authors, abstract, publishment, citation, field, price, fulltextURL)
+#         ids.append(id)
+#         succeed += 1
+#     except:
+#         print(i)
+#         print(ids)
